3cd110af10238dc9b8eb03f8a95c2c6595b64cce
Lucas Colley <lucas.colley8@gmail.com>
2025-02-02 01:11:54 +0000

    initial `Quantity` protocol

---
+++ src/quantity_api/__init__.py
@@ -0,0 +1,25 @@
+"""Quantity API."""
+
+from __future__ import annotations
+
+from typing import Any, Protocol, TypeAlias, runtime_checkable
+
+Unit: TypeAlias = Any  # TODO: unit-api
+
+__version__ = "0.0.1.dev0"
+__all__ = ["Quantity"]
+
+@runtime_checkable
+class Quantity[V, U: Unit](Protocol):
+    @property
+    def value(self) -> V: ...
+    @property
+    def unit(self) -> U: ...
+
+    def __init__(self, value: V, unit: U, **kw: Any) -> Quantity[V, U]: ...
+
+    def to_unit(self, unit: U, /, **kw: Any) -> Quantity[V, U]: ...
+
+    ### Dunder Methods
+
+    def __eq__(self, other: Quantity[V, U]): ...
