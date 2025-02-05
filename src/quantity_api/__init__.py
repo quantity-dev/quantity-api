f6445b87abdbaf4f62d633a9424626c540847c69
Lucas Colley <lucas.colley8@gmail.com>
2025-02-05 15:29:06 +0000

    improvements

--- src/quantity_api/__init__.py
+++ src/quantity_api/__init__.py
@@ -1,15 +1,13 @@
 """Quantity API."""
 
-from __future__ import annotations
-
-from typing import Any, Protocol, runtime_checkable
+from typing import Any, Final, Protocol, Self, runtime_checkable
 
 import optype as op
 
 type Unit = Any  # TODO: unit-api
 
-__version__ = "0.0.1.dev0"
-__all__ = ["Quantity"]
+__version__: Final = "0.0.1.dev0"
+__all__ = ["__version__", "Quantity"]
 
 @runtime_checkable
 class Quantity[V, U: Unit](Protocol):
@@ -21,5 +19,5 @@ class Quantity[V, U: Unit](Protocol):
     ### Dunder Methods
 
     def __eq__[B: op.CanBool](
-        self, other: Quantity[op.CanEq[V, B], U], /
+        self, other: Self[op.CanEq[V, B], U], /
     ) -> B: ...
